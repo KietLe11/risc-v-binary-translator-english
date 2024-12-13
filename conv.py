82b17f0276c658610c2d2ca7de8e4b4d5260f137
Kiet Le <ktlbusiness11@gmail.com>
2024-12-13 13:48:21 -0500

    Translate main conversion file to english so i can use it. Thanks OP

--- conv.py
+++ conv.py
@@ -3,73 +3,73 @@ from formatting import *
 
 
 def inst_type(instruction):
-    formato = ''
+    format_type = ''
     opcode = instruction[25:len(instruction) + 1]
 
-    if opcode == '1101111': formato = 'UJ'
-    elif opcode == '1100011': formato = 'SB'
-    elif opcode == '0100011': formato = 'S'
-    elif opcode == '0110111' or opcode == '0010111': formato = 'U'
-    elif opcode == '0000011' or opcode == '1100111' or opcode == '0010011': formato = 'I'
-    elif opcode == '0110011': formato = 'R'
+    if opcode == '1101111': format_type = 'UJ'
+    elif opcode == '1100011': format_type = 'SB'
+    elif opcode == '0100011': format_type = 'S'
+    elif opcode == '0110111' or opcode == '0010111': format_type = 'U'
+    elif opcode == '0000011' or opcode == '1100111' or opcode == '0010011': format_type = 'I'
+    elif opcode == '0110011': format_type = 'R'
 
-    return formato
+    return format_type
 
 
 def inst_decode(instruction, options):
-    formato = inst_type(instruction)
+    format_type = inst_type(instruction)
 
-    match formato:
+    match format_type:
         case 'R':
-            conteudos = {}
-            conteudos['funct7'], conteudos['rs2'], conteudos['rs1'], conteudos['funct3'], conteudos['rd'], conteudos['opcode'] = decode_R(instruction)
+            contents = {}
+            contents['funct7'], contents['rs2'], contents['rs1'], contents['funct3'], contents['rd'], contents['opcode'] = decode_R(instruction)
 
-            return format_R(conteudos, options)
+            return format_R(contents, options)
         case 'SB':
-            conteudos = {}
-            conteudos['imm_cycle_32'], conteudos['rs2'], conteudos['rs1'], conteudos['funct3'], conteudos['imm_offset'], conteudos['opcode'] = decode_SB(instruction)
+            contents = {}
+            contents['imm_cycle_32'], contents['rs2'], contents['rs1'], contents['funct3'], contents['imm_offset'], contents['opcode'] = decode_SB(instruction)
 
-            return format_SB(conteudos, options)
+            return format_SB(contents, options)
         case 'S':
-            conteudos = {}
-            conteudos['imm_cycle_32'], conteudos['rs2'], conteudos['rs1'], conteudos['funct3'], conteudos['imm_offset'], conteudos['opcode'] = decode_S(instruction)
+            contents = {}
+            contents['imm_cycle_32'], contents['rs2'], contents['rs1'], contents['funct3'], contents['imm_offset'], contents['opcode'] = decode_S(instruction)
 
-            return format_S(conteudos, options)
+            return format_S(contents, options)
         case 'I':
             imm, rs1, funct3, rd, opcode = decode_I(instruction)
-            conteudos = {}
-            # se é 001 ou 101 então é shift lógico e portanto tem formatação diferente
+            contents = {}
+            # If it’s 001 or 101, it’s a logical shift and therefore has different formatting
             if ((opcode == '0010011' and (funct3 != '001' and funct3 != '101')) or opcode == '0000011'):
-                conteudos['imm'] = imm
-                conteudos['rs1'] = rs1
-                conteudos['funct3'] = funct3
-                conteudos['rd'] = rd
-                conteudos['opcode'] = opcode
-            # shift lógicos tem a formatação de instruções R
+                contents['imm'] = imm
+                contents['rs1'] = rs1
+                contents['funct3'] = funct3
+                contents['rd'] = rd
+                contents['opcode'] = opcode
+            # Logical shifts have the formatting of R-type instructions
             # shamt = shift amount
             else:
                 funct7, shamt, rs1, funct3, rd, opcode = decode_R(instruction)
-                conteudos['funct7'] = funct7
-                conteudos['shamt'] = shamt
-                conteudos['rs1'] = rs1
-                conteudos['funct3'] = funct3
-                conteudos['rd'] = rd
-                conteudos['opcode'] = opcode
+                contents['funct7'] = funct7
+                contents['shamt'] = shamt
+                contents['rs1'] = rs1
+                contents['funct3'] = funct3
+                contents['rd'] = rd
+                contents['opcode'] = opcode
 
-            return format_I(conteudos, options)
+            return format_I(contents, options)
         case 'U':
-            return 'funções U e UJ não implementadas'
+            return 'U and UJ functions not implemented'
         case 'UJ':
-            return 'funções U e UJ não implementadas'
+            return 'U and UJ functions not implemented'
 
 
 def main():
-    print('interativo ou leitura de arquivo? (i/l)')
-    o1 = input()
-    if o1 == 'l':
-        print('endereço do arquivo: ')
+    print('Interactive or file reading? (i/l)')
+    choice = input()
+    if choice == 'l':
+        print('File path: ')
         address = input()
-        print('opções (sb/sx): ')
+        print('Options (sb/sx): ')
         op = input()
 
         instructions = open(address, 'r')
@@ -82,22 +82,22 @@ def main():
 
         instructions.close()
 
-    elif o1 == 'i':
-        print('as instruções serão em binário ou hexadecimal? (b/x)')
-        r = input()
-        print('opções (sb/sx): ')
+    elif choice == 'i':
+        print('Will the instructions be in binary or hexadecimal? (b/x)')
+        response = input()
+        print('Options (sb/sx): ')
         op = input()
-        match r:
+        match response:
             case 'b':
                 while True:
-                    print('instrução (para sair, digite s): ')
+                    print('Instruction (to exit, type s): ')
                     inst = input().strip()
                     if (inst == 's'): break
 
                     print(inst_decode(f'{inst:0>32}', op))
             case 'x':
                 while True:
-                    print('instrução (para sair, digite s): ')
+                    print('Instruction (to exit, type s): ')
                     inst = input().strip()
                     if (inst == 's'): break
 
